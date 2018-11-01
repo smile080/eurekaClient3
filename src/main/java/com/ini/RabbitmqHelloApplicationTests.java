abd13a5914dc2d0fa3ef7e9359b3bb4231b2c955
chen_jian <chenjian190000774@126.com>
2018-11-01 17:19:58 +0800

    20181101 17:19

---
+++ src/main/java/com/ini/RabbitmqHelloApplicationTests.java
@@ -0,0 +1,22 @@
+package com.ini;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.ini.mq.rabbit.Sender;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = RunApplicationClient3.class)
+public class RabbitmqHelloApplicationTests {
+
+    @Autowired
+    private Sender sender;
+
+    @Test
+    public void contextLoads() {
+        sender.send();
+    }
+}
